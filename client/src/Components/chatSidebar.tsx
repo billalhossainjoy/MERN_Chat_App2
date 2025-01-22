3dcc8dd64151cbe08f90ff6529e56e25bec7d70f
Billal Hossain <billalhossain.bhj@gmail.com>
2025-01-22 22:22:31 +0600

    fix upload and ui issue's

--- client/src/Components/chatSidebar.tsx
+++ client/src/Components/chatSidebar.tsx
@@ -50,7 +50,7 @@ const ChatSideBar: React.FC = () => {
               <img
                 src={user.profilePic || "./images/avatar.png"}
                 alt={user.fullName}
-                className="size-12 object-cover rounded-full ring-2"
+                className="size-12 object-cover rounded-full "
               />
               {onlineUsers.includes(user._id) && (
                 <span className="absolute bottom-0 right-0 size-3 bg-green-500 rounded-full ring-2 ring-zinc-900" />
--- client/src/store/auth.store.ts
+++ client/src/store/auth.store.ts
@@ -6,7 +6,7 @@ import {
   updateSchemaType,
 } from "../schema/auth.schema";
 import toast from "react-hot-toast";
-const date = new Date();// TODO: its for demo remove this 
+
 interface AuthState {
   authUser: IUser | null;
   isCheckingAuth: boolean;
@@ -23,102 +23,108 @@ interface AuthState {
   onlineUsers: string[];
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  authUser: null,
-  isCheckingAuth: false,
-  isSigningUp: false,
-  isUpdatingProfile: false,
-  isLoggingOut: false,
-  isLoggingIn: false,
-  onlineUsers: ["1"],
-  checkAuth: async () => {
-    set({ isCheckingAuth: true });
-    try {
-      const res = await ApiClient.get("/auth/check");
-      set({
-        authUser: res.data.data,
-      });
-    } catch (error) {
-      set({
-        authUser: null,
-      });
-    } finally {
-      set({ isCheckingAuth: false });
-    }
-  },
-  signup: async (formData) => {
-    set({ isSigningUp: true });
-    try {
-      const res = await ApiClient("/auth/signup", {
-        method: "POST",
-        data: formData,
-      });
-      set({
-        authUser: res.data.data,
-      });
-      toast.success(res.data.message);
-    } catch (error) {
-      set({
-        authUser: null,
-      });
-      console.log(error);
-    } finally {
-      set({ isSigningUp: false });
-    }
-  },
-  login: async (formData) => {
-    set({ isLoggingIn: true });
-    try {
-      const res = await ApiClient("/auth/login", {
-        method: "POST",
-        data: formData,
-      });
-      set({
-        authUser: res.data.data,
-      });
-      toast.success(res.data.message);
-    } catch (error) {
-      set({
-        authUser: null,
-      });
-      console.log(error);
-      toast.error("Login failed");
-    } finally {
-      set({ isLoggingIn: false });
-    }
-  },
-  logout: async () => {
-    set({ isLoggingOut: true });
-    try {
-      const res = await ApiClient.get("/auth/logout");
-      set({
-        authUser: null,
-      });
-      toast.success(res.data.message);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      set({ isLoggingOut: false });
-    }
-  },
-  updateProfile: async (formData) => {
-    set({ isUpdatingProfile: true });
-    try {
-      const res = await ApiClient("/user/update-profile", {
-        method: "POST",
-        data: formData,
-      });
-      set({
-        authUser: res.data.data,
-      });
-      toast.success(res.data.message);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      set({
-        isUpdatingProfile: false,
-      });
-    }
-  },
-  connectSocket: async () => {},
-}));
+export const useAuthStore = create<AuthState>(
+  (set) => ({
+    authUser: null,
+    isCheckingAuth: false,
+    isSigningUp: false,
+    isUpdatingProfile: false,
+    isLoggingOut: false,
+    isLoggingIn: false,
+    onlineUsers: ["1"],
+    checkAuth: async () => {
+      set({ isCheckingAuth: true });
+      try {
+        const res = await ApiClient.get("/auth/check");
+        set({
+          authUser: res.data.data,
+        });
+      } catch (error) {
+        set({
+          authUser: null,
+        });
+      } finally {
+        set({ isCheckingAuth: false });
+      }
+    },
+    signup: async (formData) => {
+      set({ isSigningUp: true });
+      try {
+        const res = await ApiClient("/auth/signup", {
+          method: "POST",
+          data: formData,
+        });
+        set({
+          authUser: res.data.data,
+        });
+        toast.success(res.data.message);
+      } catch (error) {
+        set({
+          authUser: null,
+        });
+        console.log(error);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
+    login: async (formData) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await ApiClient("/auth/login", {
+          method: "POST",
+          data: formData,
+        });
+        set({
+          authUser: res.data.data,
+        });
+        toast.success(res.data.message);
+      } catch (error) {
+        set({
+          authUser: null,
+        });
+        console.log(error);
+        toast.error("Login failed");
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+    logout: async () => {
+      set({ isLoggingOut: true });
+      try {
+        const res = await ApiClient.get("/auth/logout");
+        set({
+          authUser: null,
+        });
+        toast.success(res.data.message);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        set({ isLoggingOut: false });
+      }
+    },
+    updateProfile: async (formData) => {
+      set({ isUpdatingProfile: true });
+
+      try {
+        const res = await ApiClient("/user/update-profile", {
+          method: "POST",
+          data: formData,
+        });
+
+        set((prev) => ({
+          authUser: prev.authUser
+            ? { ...prev.authUser, profilePic: res.data.data.profilePic }
+            : null,
+        }));
+        toast.success(res.data.message);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        set({
+          isUpdatingProfile: false,
+        });
+      }
+    },
+    connectSocket: async () => {},
+  })
+);
